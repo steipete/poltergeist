4cbee8eff9be8e2f0ef55919e2de5ca9ea2fb456
Peter Steinberger <steipete@gmail.com>
2025-11-16 17:29:42 +0100

    chore(cli): back out descriptor option wiring

--- src/cli/commands/index.ts
+++ src/cli/commands/index.ts
@@ -1,10 +1,6 @@
 import type { Command } from 'commander';
 import { COMMAND_DESCRIPTORS } from './registry.js';
-import { applyDescriptorOptions } from './options-descriptors.js';
 
 export const registerCliCommands = (program: Command): void => {
-  COMMAND_DESCRIPTORS.forEach((descriptor) => {
-    const scopedProgram = applyDescriptorOptions(program, descriptor);
-    descriptor.register(scopedProgram);
-  });
+  COMMAND_DESCRIPTORS.forEach(({ register }) => register(program));
 };
